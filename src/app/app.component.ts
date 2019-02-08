1bd1036278423c014e3eb0ba228a8c1df3bcaea5
sanzmoses <sanzmoses@gmail.com>
2019-02-08 20:15:48 +0800

    almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   input: string = '';
+  hartHart: number = 0;
+  edit = {
+    index: 0,
+    editing: false
+  };
+  cards: { datetime: string, complaint: string }[] = [
+    { 'datetime': '12-31-1990, 3:15', 'complaint': 'Y u do diz to meh?'},
+    { 'datetime': '12-31-2018, 10:20', 'complaint': 'I dun fel gud mr Stork!'}
+  ];
 
   refreshWhine() {
     this.input = '';
+    this.edit.editing = false;
     console.log('refreshing...');
   }
+
+  newWhine() {
+    if(this.input == '') {
+      alert('Everybodys got some problems!');
+      return ;
+    }
+
+    let now = new Date();
+    let stamp;
+    // console.log(now);
+    stamp = now.getMonth()+1 +"-"+ now.getDate() + "-"+now.getFullYear()+", "+now.getHours()+":"+now.getMinutes();
+
+    let newObj = {
+      datetime: stamp,
+      complaint: this.input
+    };
+
+    if(this.edit.editing) {
+      this.cards[this.edit.index] = newObj;
+      this.edit.editing = false;
+      this.input = '';
+    }
+    else {
+      this.cards.push(newObj);
+      this.input = '';
+    }
+  }
+
+  receiveHart($event) {
+    ($event) ? this.hartHart++ : this.hartHart--;
+  }
+
+  update($event) {
+    if($event.type == 'delete') {
+      this.cards.splice($event.index, 1);
+      //if the post has hart-hart
+      if ($event.hart) this.hartHart--;
+    }
+    else {
+      this.input = $event.whine;
+      this.edit.editing = true;
+      this.edit.index = $event.index;
+    }
+  }
+
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() whimper: string
+  @Input() datetime: string
+  @Input() index: number
+  @Output() hartEvent: any = new EventEmitter();
+  @Output() editDelete: any = new EventEmitter();
+  hartHart = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loveIt() {
+    this.hartHart = !this.hartHart;
+    this.hartEvent.emit(this.hartHart);
+  }
+
+  change(type) {
+    this.editDelete.emit({'type':type, 'index': this.index, 'hart': this.hartHart, 'whine': this.whimper});
   }
 
 }
